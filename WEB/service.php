e6f254b9eadbdaa97a097d93b871480cbd010dab
Leakune <lfavier@live.fr>
2020-02-19 12:05:30 +0100

    Add service.php

---
+++ WEB/service.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!--<link href="main.css" rel="stylesheet">-->
+    <title>Services</title>
+    <link rel="shortcut icon" href="../image/logo.png">
+    <script src="barre.js"></script>
+  </head>
+  <body>
+      <h1>Service</h1>
+  </body>
+</html>
