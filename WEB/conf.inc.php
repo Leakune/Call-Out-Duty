556054954bc51736016662475f57c709253d4008
medelmat <mohamed.elmatror@gmail.com>
2020-03-28 17:08:45 +0100

    update stripe v2

--- WEB/conf.inc.php
+++ WEB/conf.inc.php
@@ -1,8 +1,21 @@
 <?php
 
+// Subcription plans
+
+
+
+
+
+
+// Database configuration
+
 define("DBDRIVER", "mysql");
 define("DBHOST", "localhost");
-define("DBNAME", "call-out-duty p1");
+define("DBNAME", "projet_annuel");
 define("DBPORT", "3308");
 define("DBUSER", "root");
 define("DBPWD", "root");
+ 
+// STRIPE API CONFIGURATION
+define('STRIPE_API_KEY', 'sk_test_jYyNTsbTFHI8H0iZAdF4Tg4r00JIUz3Y50');
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_TkxT5Z5Lo6oueLmqNWil7QHS00AjJMbgra'); 
--- WEB/login-home/buy-subscriptions.php
+++ WEB/login-home/buy-subscriptions.php
@@ -1,3 +1,10 @@
+<?php
+
+	require_once '../conf.inc.php';
+				
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -53,6 +60,9 @@
 		.StripeElement--webkit-autofill {
 			background-color: #fefde5 !important;
 		}
+		#cbContain{
+			width: 100%;
+		}
 	</style>
 </head>
 <body>
@@ -70,7 +80,8 @@
 
 					<?php
 						session_start();
-						echo "Bonjour ".$_SESSION['firstname']." !";
+					//	echo "Bonjour ".$_SESSION['firstname']." !";
+						
 
 					 ?>
 
@@ -161,118 +172,210 @@
 
 
 		<!-- formulaire -->
+		<div class="container">
+			<div class="row pt-3 px-4">
+      		<form action="payment.php" method="POST" id="paymentFrm">
+      			<div class="form-group">
+      				
+      					<label>Sélectionner un abonnement</label>
+      					<select name="subscr_plan" id="subscr_pan" class="form-control">
+      					<option selected>Choisir...</option>	
+      					<?php
+      					include "../functions.php";
+      					$connect=connectDb();
+      					$query= "SELECT name, price, intervaltime FROM subscription_offer";
+      					$result=$connect->query($query);
+      					foreach ($result as $id => $plan) {
+      					
+      					echo "<option value=\"".$id."\">".$plan['name']."[".$plan['price']."€/".$plan['intervaltime']."]</option>";
+      				}
+      					?>
 
-		<div class="contain">
-
-			<div class="container">
-						<div class="card-body p-0">
-							<!-- Nested Row within Card Body -->
-									<div class="p-5">
-										<div class="text-center">
-											<h1 id="add" class="h4 text-gray-900 mb-4">Souscrire à un abonnement</h1>
-										</div>
-
-				<form action="/charge" method="POST" id="payment-form">
-					<div class="form-row">
-
-						<div class="col-md-8 mb-4" id ="card-element">
-							<label for="card-element">Credit or debit card</label>
-							<div id="card-element">
-		      					<!-- A Stripe Element will be inserted here. -->
-		    					</div>
-
-							<!-- Used to display form errors. -->
-		    					<div id="card-errors" role="alert"></div>
-		  					</div>
-						</div>
-
-					</div>
-
-					<button class="btn btn-primary">Submit Payment</button>
-				</form>
-
-		</div>
-	</div>
-</div>
-</div>
-
-</div>
+      						
+      					</select>
+      				
+      			</div>
+      			<div class="form-row">
+      				<!-- Display errors -->
+      				<div class="card-errors"></div>
+      				<!-- Payment form -->
+      				<div class= "form-group col-md-6">
+      					<label>Nom</label>
+      					<input type="text" name="name" id="name" placeholder="Votre nom" required="" autofocus="" class="form-control">
+      				</div>
+      				<div class="form-group col-md-6">
+      					<label>Email</label>
+      					<input type="email" name="email" id="email" placeholder="Votre mail" required="" class="form-control">
+      				</div>
+      			</div>
+      			<div >
+      				<label style="width: 100%;" >
+      					<span><span>Carte bancaire</span></span>
+      					<div id="card-element" class="field is-empty" ></div>
+      					
+      				</label>
+      			</div>
+      				
+      		<!--		<div class="form-group">
+      					<label>Numéro de votre Carte Bancaire</label>
+      					<input type="text" name="card_number" id="card_number" placeholder="1234 1234 1234 1234" maxlength="16"	 autocomplete="off" required="">
+      				</div>
+      				<div class="row">
+      					<div class="left">
+      						<div class="form-group">
+      							<label>Date d'expiration</label>
+      							<div class="col-1">
+      								<input type="text" name="card_exp_month" id="card_exp_month" placeholder="MM" maxlength="2" required="">
+      							</div>
+      							<div class="col-2">
+      								<input type="text" name="card_exp_year" id="card_exp_year" placeholder="YYYY" maxlength="4" required="">
+      							</div>
+      						
+      						</div>
+      					</div>
+      					<div class="right">
+      						<div class="form-group">
+      							<label>CVC code</label>
+      							<input type="text" name="card_cvc" id="card_cvc" placeholder="CVC" maxlength="3" autocomplete="off" required="">     					
+      						</div>
+      						
+      					</div> -->
+      			</div>
+      			<button type="submit" class="btn btn-success" id="payBtn">Valider paiement
+      				
+      			</button>
+      			
+      		</form>
+      </div>
+  </div>
+      
 
 
 	<script src="../barre.js"></script>
   <script src="https://js.stripe.com/v3/"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="front-login.js"></script>
 	<script type="text/javascript">
 		// Create a Stripe client.
-		var stripe = Stripe('pk_test_b0MvnRqWoapubW770qawT2GX0085l1sVTd');
+		 var stripe= Stripe('<?php echo STRIPE_PUBLISHABLE_KEY ?>');
+		 var elements= stripe.elements();
 
-		// Create an instance of Elements.
-		var elements = stripe.elements();
+		var card = elements.create('card', {
+			  'style': {
+			    'base': {
+			      'fontFamily': 'Arial, sans-serif',
+			      
+			      'color': '#C1C7CD',
+			    },
+			    'invalid': {
+			      'color': 'red',
+			    },
+			  }
+			});
+		card.mount('#card-element');
+		
 
-		// Custom styling can be passed to options when creating an Element.
-		// (Note that this demo uses a wider set of styles than the guide below.)
-		var style = {
-			base: {
-				color: '#32325d',
-				fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-				fontSmoothing: 'antialiased',
-				fontSize: '16px',
-				'::placeholder': {
-					color: '#aab7c4'
-				}
-			},
-			invalid: {
-				color: '#fa755a',
-				iconColor: '#fa755a'
-			}
+
+		function stripeTokenHandler(token) {
+		  // Insert the token ID into the form so it gets submitted to the server
+		  var form = document.getElementById('paymentFrm');
+		  var hiddenInput = document.createElement('input');
+		  hiddenInput.setAttribute('type', 'hidden');
+		  hiddenInput.setAttribute('name', 'stripeToken');
+		  hiddenInput.setAttribute('value', token.id);
+		  form.appendChild(hiddenInput);
+
+		  // Submit the form
+		  form.submit();
+		}
+
+		function subscrPlanHandler(value) {
+		  // Insert the token ID into the form so it gets submitted to the server
+		  var form = document.getElementById('paymentFrm');
+		  var hiddenInput = document.createElement('input');
+		  hiddenInput.setAttribute('type', 'hidden');
+		  hiddenInput.setAttribute('name', 'subscr_pan');
+		  hiddenInput.setAttribute('value', value);
+		  form.appendChild(hiddenInput);
+
+		
+		}
+
+		function createToken() {
+		  stripe.createToken(card).then(function(result) {
+		    if (result.error) {
+		      // Inform the user if there was an error
+		      var errorElement = document.getElementById('card-errors');
+		      errorElement.textContent = result.error.message;
+		    } else {
+		      // Send the token to your server
+		      stripeTokenHandler(result.token);
+		    }
+		  });
 		};
 
-		// Create an instance of the card Element.
-		var card = elements.create('card', {style: style});
+		// Create a token when the form is submitted.
+		var form = document.getElementById('paymentFrm');
+		form.addEventListener('submit', function(e) {
+		  e.preventDefault();
+		  var sub= document.getElementById('subscr_pan');
+		  var subscr_pan=sub.options[sub.selectedIndex].value;
+		  subscrPlanHandler(subscr_pan);
+		  createToken();
+		});
 
-		// Add an instance of the card Element into the `card-element` <div>.
-		card.mount('#card-element');
-
-		// Handle real-time validation errors from the card Element.
 		card.addEventListener('change', function(event) {
-			var displayError = document.getElementById('card-errors');
-			if (event.error) {
-				displayError.textContent = event.error.message;
-			} else {
-				displayError.textContent = '';
+		  var displayError = document.getElementById('card-errors');
+		  if (event.error) {
+		    displayError.textContent = event.error.message;
+		  } else {
+		    displayError.textContent = '';
+		  }
+		});
+
+/*
+		// Callback to handle the response from stripe
+		function stripeResponseHandler(status, response) {
+    		if (response.error) {
+       		 // Enable the submit button
+       		 $('#payBtn').removeAttr("disabled");
+       		 // Display the errors on the form
+        	$(".payment-status").html('<p>'+response.error.message+'</p>');
+    		} else {
+        	var form$ = $("#paymentFrm");
+        	// Get token id
+       	 	var token = response.id;
+       	 	// Insert the token into the form
+       	 	var hiddenInput = document.createElement('input');
+    		hiddenInput.setAttribute('type', 'hidden');
+    		hiddenInput.setAttribute('name', 'stripeToken');
+    		hiddenInput.setAttribute('value', token);
+       	 	form$.appendChild(hiddenInput);
+       	 	console.log(token);
+        	// Submit form to the server
+        	form$.submit();
+    				}
 			}
-		});
 
-		// Handle form submission.
-		var form = document.getElementById('payment-form');
-		form.addEventListener('submit', function(event) {
-			event.preventDefault();
-
-			stripe.createToken(card).then(function(result) {
-				if (result.error) {
-					// Inform the user if there was an error.
-					var errorElement = document.getElementById('card-errors');
-					errorElement.textContent = result.error.message;
-				} else {
-					// Send the token to your server.
-					stripeTokenHandler(result.token);
-				}
+			$(document).ready(function() {
+    		// On form submit
+   			 $("#paymentFrm").submit(function() {
+     			   // Disable the submit button to prevent repeated clicks
+        	$('#payBtn').attr("disabled", "disabled");
+		
+        	// Create single-use token to charge the user
+        	stripe.createToken({
+            number: $('#card_number').val(),
+            exp_month: $('#card_exp_month').val(),
+            exp_year: $('#card_exp_year').val(),
+            cvc: $('#card_cvc').val()
+       		 }, stripeResponseHandler);
+		
+        	// Submit from callback
+        	return false;
+    			});
 			});
-		});
-
-		// Submit the form with the token ID.
-		function stripeTokenHandler(token) {
-			// Insert the token ID into the form so it gets submitted to the server
-			var form = document.getElementById('payment-form');
-			var hiddenInput = document.createElement('input');
-			hiddenInput.setAttribute('type', 'hidden');
-			hiddenInput.setAttribute('name', 'stripeToken');
-			hiddenInput.setAttribute('value', token.id);
-			form.appendChild(hiddenInput);
-
-			// Submit the form
-			form.submit();
-		}
+*/
 	</script>
 </body>
 </html>
